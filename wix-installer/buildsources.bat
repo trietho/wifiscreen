a6ee570a65a6efe384f853f55017732fbd2e7966
Triet Ho <hhtriet@msn.com>
2017-12-07 16:04:33 +0700

    Removed wix-installer Added Screenshoots

--- wix-installer/buildsources.bat
+++
@@ -1,5 +0,0 @@
-call vcvarsall.bat
-
-vcbuild.exe /rebuild /M%NUMBER_OF_PROCESSORS% "..\..\src\TightVNC.sln" "Release|Win32"
-vcbuild.exe /rebuild /M%NUMBER_OF_PROCESSORS% "..\..\src\TightVNC.sln" "Release|x64"
-
